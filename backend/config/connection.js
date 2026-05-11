2b43a97388429cb637c1f21502a79701790d09e8
sudev <sudev1997@gmail.com>
2026-05-11 23:14:12 +0530

    MONGODB_URI ip

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -3,7 +3,9 @@ const URL = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(URL);
+    const conn = await mongoose.connect(URL,{
+      family: 4, // Use IPv4 to avoid potential issues with IPv6
+    });
     console.log(`Database connected successfully: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Database connection error: ${error}`);
